8aea3101b38e0e7877537249522e84d6e5faccac
Sabrina Li <sabrinali0312@gmail.com>
2019-05-16 21:39:59 -0400

    time axis

---
+++ public/js/dummy_data.js
@@ -0,0 +1,29 @@
+const dummy_data = {
+    stool:[
+        {
+            bristolScore:1,
+            time:"05/14/2019 14:00",
+            comments:"ewwwww"
+        },{
+            bristolScore:4,
+            time:"05/16/2019 14:00",
+            comments:"interesting..."
+        },{
+            bristolScore:6,
+            time:"05/17/2019 14:00",
+            comments:"???"
+        }
+    ],
+    water:[
+        {
+            intake:100,
+            time:"05/16/2019 01:00",
+        },{
+            intake:400,
+            time:"05/16/2019 10:00",
+        },{
+            intake:160,
+            time:"05/17/2019 12:00",
+        }
+    ]
+}
