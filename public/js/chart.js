a116d7f1a0ded51f302d004fda4f028e1cd84755
Sabrina Li <sabrinali0312@gmail.com>
2019-05-16 21:39:59 -0400

    time axis

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,89 +1,127 @@
-var ctx = document.getElementById('poopChart').getContext('2d');
+const displayChart = _ => {
 
-const lightBlue = 'rgba(54, 162, 235, 0.2)',
-    blue = 'rgba(54, 162, 235, 1)',
-    lightOrange = 'rgba(255, 159, 64, 0.2)',
-    orange = 'rgba(255, 159, 64, 1)';
-const dates=[1,2,3,4];
-const bristolScores=[5,4,6,7]
+    const ctx = document.getElementById('poopChart').getContext('2d');
+
+    const lightBlue = 'rgba(54, 162, 235, 0.2)',
+        blue = 'rgba(54, 162, 235, 1)',
+        lightOrange = 'rgba(255, 159, 64, 0.2)',
+        orange = 'rgba(255, 159, 64, 1)';
+
+    const timeFormat = 'MM/DD/YYYY HH:mm';
+    moment.defaultFormat = timeFormat;
+
+    const waterData = dummy_data.water;//from dummy_Data
+    const waterIntake = waterData.map(x => x.intake);
+    const waterTimes = waterData.map(x => x.time);
+    const stoolData = dummy_data.stool;//from dummy_Data
+    const bristolScores = stoolData.map(x => {
+        let result = {};
+        result.y = x.bristolScore;
+        result.x = x.time;
+        
+        return result;
+    });
+    console.log(waterTimes);
+    console.log(bristolScores);
 
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: dates,
-        datasets: [{
-            label: '# of Votes',
-            data: [120, 190, 30, 50],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 1,
-            yAxisID:"y-water"
-        },{
-            label: 'bristol score',
-            data: bristolScores,
-            type: 'line',
-            yAxisID:"y-bristol"
-        }]
-    },
-    options:{
-        responsive: true,
-        stacked: false,
-        hoverMode: 'index',
-        title:{
-            display: true,
-            text:'Chart.js Line Chart - Multi Axis'
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: waterTimes,
+            datasets: [
+                {
+                    label: 'Water Intake',
+                    data: waterIntake,
+                    backgroundColor: lightBlue,
+                    borderColor: blue,
+                    borderWidth: 1,
+                    yAxisID: "y-water"
+                },{
+                    label: 'bristol score',
+                    data: bristolScores,
+                    backgroundColor:[lightOrange],
+                    borderColor:[orange],
+                    type: 'line',
+                    yAxisID:"y-bristol"
+                }
+            ]
         },
-        scales: {
-            yAxes: [{
-                type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+        options: {
+            responsive: true,
+            stacked: false,
+            hoverMode: 'index',
+            title: {
                 display: true,
-                position: "left",
-                id: "y-water",
-            }, {
-                min:0,
-                max:8,
-                type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                display: true,
-                position: "right",
-                id: "y-bristol",
-                
+                text: 'Stool Stats'
+            },
 
-                // grid line settings
-                gridLines: {
-                    drawOnChartArea: false, // only want the grid lines for one axis to show up
-                },
-            }],
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        parser: timeFormat,
+                        round: 'hour',
+                        
+                        tooltipFormat: 'll HH:mm',
+                        // displayFormats: {
+                        //     // day:'MMM D',
+                        //     minute:'h:mm a'
+                        // }
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }],
+                yAxes: [{
+                    type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: "left",
+                    id: "y-water",
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0
+                    }
+                }, {
+                    type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: "right",
+                    id: "y-bristol",
+
+                    // grid line settings
+                    gridLines: {
+                        drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0
+                    }
+                }],
+            }
+
         }
+    });
 
-    }
-});
+    return myChart;
+}
 
 
-// var mixedChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         datasets: [{
-//             label: 'Bar Dataset',
-//             data: [10, 20, 30, 40]
-//         }, {
-//             label: 'Line Dataset',
-//             data: [50, 50, 50, 50],
 
-//             // Changes this dataset to become a line
-//             type: 'line'
-//         }],
-//         labels: ['January', 'February', 'March', 'April']
-//     },
-//     options: options
-// });
+
+
+
+const timeFormat = "YYYY-DD-MM HH:mm"
+
+function newDate(days) {
+    return moment().add(days, 'd').toDate();
+}
+
+function newDateString(days) {
+    return moment().add(days, 'd').format(timeFormat);
+}
+console.log(newDate(0))
+console.log(newDateString(0));
+
+displayChart();
+
