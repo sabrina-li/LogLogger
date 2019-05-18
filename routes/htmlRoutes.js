82c3e9e67059ae11ce8109cfb22d712955f51d23
Sabrina Li <sabrinali0312@gmail.com>
2019-05-18 12:42:14 -0400

    add modal

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,20 @@
 var db = require("../models");
+const path = require("path");
 
+//TODO, read from DB to show the uer info
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // Load index page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname,"../views/index.html"));
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    // Load example page and pass in an example by id
+    app.get("/users/:userid", function(req, res) {
+        res.sendFile(path.join(__dirname,"../views/data.html"));
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        res.render("404");
+    });
 };
