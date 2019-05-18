d45cafb6616947f867303e9b85172176777f04eb
Sabrina Li <sabrinali0312@gmail.com>
2019-05-18 13:58:22 -0400

    water modal

--- public/js/data.js
+++ public/js/data.js
@@ -1,17 +1,24 @@
-$(document).ready(_=>{
+$(document).ready(_ => {
     const $waterAddBtn = $(".waterAddBtn");
     const $stoolAddBtn = $(".stoolAddBtn");
-    
+    $(document).ready(function () {
+        $('.datepicker').datepicker();
+    });
+    $(document).ready(function () {
+        $('select').formSelect();
+    });
 
-    $("#waterAddBtn").on("click",function(){
+
+    $("#waterAddBtn").on("click", function () {
         console.log("clicked");
         console.log($(this).attr("id"));
     });
 
 
-    $(".sub-button").on("mouseover",function(event){
+    $(".sub-button").on("mouseover", function (event) {
         $('.modal').modal();
         console.log("hover");
         $(this).addClass("modal-trigger");
     });
+
 });
