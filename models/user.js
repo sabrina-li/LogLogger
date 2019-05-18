2654938d44969fd8f1834bdb62759f9436fc9de0
Darkangel <wtsegars@mac.com>
2019-05-18 14:14:48 -0400

    fixing merges from master

--- models/user.js
+++ models/user.js
@@ -1,23 +1,23 @@
 module.exports = function(sequelize, Sequelize) {
-  var User = sequelize.define("users", {
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    } 
-  });
-
-  User.associate = function(models) {
-    User.hasMany(models.Stool, {
-      onDelete: "cascade"
+    var User = sequelize.define("users", {
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        }
     });
-  };
 
-  return User;
+    User.associate = function(models) {
+        User.hasMany(models.Stool, {
+            onDelete: "cascade"
+        });
+    };
+
+    return User;
   
-}
+};
