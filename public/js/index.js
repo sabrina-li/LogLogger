75fd88376ef1ec4ec8d5e59491c5dbb0bd190c4d
Sabrina Li <sabrinali0312@gmail.com>
2019-05-18 11:36:39 -0400

    adding rules for eslint

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,25 @@
 $(document).ready(_=>{
     var $loginBtn = $("#loginBtn");
-    var $signupBtn = $("#example-signupBtn");   
-    
+    var $signupBtn = $("#example-signupBtn");
+
     $loginBtn.on('click',event=>{
         event.preventDefault();
         const user = {
             username:$("#username").val(),
             password:$("#password").val()
-        }
+        };
         API.login(user).then(result =>{
             console.log(result);
-        })
+        });
     });
     $signupBtn.on('click',event=>{
         event.preventDefault();
         const user = {
             username:$("#username").val(),
             password:$("#password").val()
-        }
+        };
         API.signup(user).then(result =>{
             console.log(result);
-        })
-    })
-})
+        });
+    });
+});
