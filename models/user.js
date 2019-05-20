cb6ec039ba8436679f677d0136e3527610a689ac
Darkangel <wtsegars@mac.com>
2019-05-20 15:53:57 -0400

    Removed Unique from models

--- models/user.js
+++ models/user.js
@@ -3,12 +3,10 @@ module.exports = function(sequelize, Sequelize) {
     username: {
       type: Sequelize.STRING,
       allowNull: false,
-      unique: true
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
-      unique: true
     } 
   });
 
