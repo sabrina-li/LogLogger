c54fd2294eeeba2569d02d976760ea59d759f1cc
Sabrina Li <sabrinali0312@gmail.com>
2019-05-21 14:35:10 -0400

    api routes using router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
 const db = require("../models");
 const moment = require("moment");
 
+const express = require('express');
+const apiRouter = express.Router();//for api routes
+
 
 const convertTime = body=>{
     console.log(body);
@@ -13,12 +16,20 @@ const convertTime = body=>{
     return datetime.format("YYYY-MM-DD HH:mm:ss");
 };
 
-const userId = 1;
+const checkAuth = user=>{
+    if(user && user.id && !isNaN(parseInt(user.id))) {//validate user ID
+        return parseInt(user.id);
+        //TODO: check against DB
+    }else{
+        throw 401;//TODO  throw the stack  and proper error
+    }
+};
 
-module.exports = function (app) {
-    // TODO Get all data from an user
-    // Get all examples
-    app.get("/api/alldata", function(req, res) {
+// Get all data from current user
+apiRouter.get("/alldata", (req, res, next)=> {
+    try{
+        const userId = checkAuth(req.user);
+
         db.User.findOne({
             where:{
                 id:userId
@@ -26,7 +37,7 @@ module.exports = function (app) {
         }).then(function(userResult) {
             let stoolPromise = userResult.getStools();
             let waterPromise = userResult.getWaters();
-            
+
             Promise.all([stoolPromise,waterPromise]).then(results=>{
                 let result={};
                 result.stool = results[0];
@@ -34,13 +45,16 @@ module.exports = function (app) {
                 res.send(result);
             });
         });
-    });
+    }catch(err){
+        next(err);
+    }
+});
 
 
-    //post a new stool log for user
-    app.post("/api/stool", function (req, res) {
-        //TODO: check for authenticated or not? if not throw error 401!
-
+//post a new stool log for user
+apiRouter.post("/stool", function (req, res, next) {
+    try{
+        const userId = checkAuth(req.user);
         if (req.body && req.body.score && !isNaN(parseInt(req.body.score))) {
             db.User.findOne({
                 where: {
@@ -58,12 +72,15 @@ module.exports = function (app) {
         } else {
             res.status(400).send("invalid input");
         }
-    });
+    }catch(err){
+        next(err);
+    }
+});
 
 
-    app.post("/api/water", function(req, res) {
-        //TODO: check for authenticated or not? if not throw error 401!
-
+apiRouter.post("/water", function(req, res) {
+    try{
+        const userId = checkAuth(req.user);
         if(req.body && req.body.intake && !isNaN(parseInt(req.body.intake))){
             db.User.findOne({
                 where:{
@@ -80,18 +97,24 @@ module.exports = function (app) {
         }else{
             res.status(400).send("invalid input");
         }
-    });
+    }catch(err){
+        next(err);
+    }
+});
 
-    // TODO: Delete an user by id
-    app.delete("/api/users/:id", function (req, res) {
-        //TODO: check for authenticated or not?
+// TODO: Delete an user by id
+apiRouter.delete("/api/users/:id", function (req, res) {
+    //TODO: check for authenticated or not?
 
-        // Delete an example by id
-        app.delete("/api/examples/:id", function (req, res) {
+    // Delete an example by id
+    app.delete("/api/examples/:id", function (req, res) {
 
-            db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-                res.json(dbExample);
-            });
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+            res.json(dbExample);
         });
     });
-};
+});
+
+
+
+module.exports = router;
