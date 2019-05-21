1342463b418287d3eb8ffea5816e155d1d2c491f
Sabrina Li <sabrinali0312@gmail.com>
2019-05-21 15:30:32 -0400

    helper convertTime method test case

--- test/apiRoutes.test.js
+++ test/apiRoutes.test.js
@@ -1,13 +1,30 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const apitRoutes = require("../routes/apiRoutes");
+const Helper = require("../routes/utils/Helper");
 
+const req1={
+    body:{
+        intake:100,
+        date:"May 01, 2019",
+        hour:5,
+        ampm:"PM",
+    }
+};
+const req2={
+    body:{
+        intake:100,
+        date:"May 31, 2019",
+        hour:3,
+        ampm:"AM",
+    }
+};
 
-describe("get /api/alldata from a user",(done)=>{
-    it("should return both water and stool data",()=>{
-        let app;
-        apitRoutes(app);
-        console.log(app);
+describe("helper class for converting time and check auth",(done)=>{
+    it("should return AM in YYYY-MM-DD HH:mm:ss format",()=>{
+        expect(Helper.convertTime(req1.body)).to.equal("2019-05-01 17:00:00");
+    });
+    it("should return PM in 24 hour clock format",()=>{
+        expect(Helper.convertTime(req2.body)).to.equal("2019-05-31 03:00:00");
     });
 });
