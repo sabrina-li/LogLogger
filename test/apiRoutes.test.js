f3a8990c9b52a357c24a625acf4db5a7cb53d29a
Sabrina Li <sabrinali0312@gmail.com>
2019-05-21 14:17:28 -0400

    first chai test

---
+++ test/apiRoutes.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const apitRoutes = require("../routes/apiRoutes");
+
+
+describe("get /api/alldata from a user",(done)=>{
+    it("should return both water and stool data",()=>{
+        let app;
+        apitRoutes(app);
+        console.log(app);
+    });
+});
