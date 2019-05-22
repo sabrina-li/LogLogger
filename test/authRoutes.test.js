1dfe414d3f4640626fb17022759ac06ad551c741
Sabrina Li <sabrinali0312@gmail.com>
2019-05-22 18:44:01 -0400

    try login test

--- test/authRoutes.test.js
+++ test/authRoutes.test.js
@@ -1,32 +1,52 @@
 process.env.NODE_ENV = "test";
-let app = require("../server");
+const app = require("../server");
 
-let chai = require("chai");
-let chaiHttp = require("chai-http");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const db = require("../models");
 
 chai.use(chaiHttp);
 //Our parent block
 describe("Auth APIs", () => {
-    // beforeEach((done) => { 
-    //     //TODO Before each test empty the database     
-    // });
-
-    describe("POST /signup new user", () => {
-        it("should create a user in db", (done) => {
-            let user = {
-                username: "test",
-                password: "test"
-            };
-            chai.request(app)
-                .post("/signup")
-                .send(user)
-                .end((err, res) => {
-                    expect(err).to.be.null;
-                    expect(res.status).to.equal(200);
-                    done();
-                });
+    beforeEach((done) => {
+        //Create a dummy user
+        db.User.create({
+            username: "test",
+            password: "fakepassword"
+        }).then(() => {
+            done();
+        }).catch(err => {
+            done();
         });
     });
+
+    const newUser = {
+        username: "testUser",
+        password: "testPass"
+    };
+
+    it("should return 200 for signup page", (done) => {
+        chai.request(app)
+            .post("/signup")
+            .send(newUser)
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
+
+    it("should return 200 for login page", (done) => {
+        chai.request(app)
+            .post("/login")
+            .send(user)
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
+
 
 });
